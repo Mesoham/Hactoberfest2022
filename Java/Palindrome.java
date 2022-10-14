c4ba216b38f8126b69c8d5766dd9f8dbf6977fb6
Soham Chowdhury <chowdhurysoham007@gmail.com>
2022-10-14 20:48:55 +0530

    Update Palindrome.java

--- Java/Palindrome.java
+++ Java/Palindrome.java
@@ -1,45 +1,17 @@
-// Statement : A palindrome is a word, phrase, number, or other sequence of characters which reads the same backward or forward.
-// If given String is palindrome output Yes otherwise No.
-// Input:hactoberfest // output:No
-// Input : madam // output:Yes
-
-import java.io.*;
-import java.util.*;
-
-public class Palindrome{
-
-    public static void main(String[] args) {
-        
-        Scanner sc=new Scanner(System.in);
-        String A=sc.next();
-        int t=A.length();
-        
-        if(t%2==0)
-        { int sum=0;
-           for(int x=0; x<=(t/2)-1; x++)
-           {
-               if(A.charAt(x)==A.charAt(t-(x+1)))
-               sum++;
-           }
-           if(sum==t/2)
-           System.out.println("Yes");
-           else
-           System.out.println("No");
-        }
-        else
-        {
-            int add=0;
-            for(int y=0; y<(t-1)/2; y++)
-            {
-                if(A.charAt(y)==A.charAt(t-(y+1)))
-                add++;
-            }
-            if(add==(t-1)/2)
-            System.out.println("Yes");
-            else
-            System.out.println("No");
-        }
-        
-    }
-}
-
+class PalindromeExample{  
+ public static void main(String args[]){  
+  int r,sum=0,temp;    
+  int n=454;//It is the number variable to be checked for palindrome  
+  
+  temp=n;    
+  while(n>0){    
+   r=n%10;  //getting remainder  
+   sum=(sum*10)+r;    
+   n=n/10;    
+  }    
+  if(temp==sum)    
+   System.out.println("palindrome number ");    
+  else    
+   System.out.println("not palindrome");    
+}  
+}  
